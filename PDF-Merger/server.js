2cb2d6f8777bd648f312ba77b161fa2864568f68
Shubham Jha <jhashubham059@gmail.com>
2025-12-28 18:51:11 +0530

    Initial commit

---
+++ PDF-Merger/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import PDFMerger from 'pdf-merger-js';
+import multer from "multer";
+import fs from 'fs';
+
+const upload = multer({ dest: 'uploads/' })
+
+var merger = new PDFMerger();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+const merge = async (file1, file2)=>{
+    // PDFMerger starts fresh each time so previous files doesn't merge in it
+    let merger = new PDFMerger();
+    await merger.add(file1);
+    await merger.add(file2);
+    await merger.save('merged.pdf');
+    // Delete the uploaded files after merging.
+    fs.unlinkSync(file1);
+    fs.unlinkSync(file2);
+};
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+    // Delete the previously created merged file
+    fs.unlink("merged.pdf", (err) => {
+        if (err && err.code !== 'ENOENT') {
+            console.error("Error deleting merged.pdf:", err);
+        } else {
+            console.log("merged.pdf deleted (if existed).");
+        }
+    });
+    
+});
+
+app.get("/about", (req, res) =>{
+    res.render("about.ejs");
+});
+
+app.post("/merge", upload.array('pdfs', 2), async function (req, res, next) {
+    await merge(req.files[0].path, req.files[1].path);
+    res.download('merged.pdf');
+  });
+
+app.listen(port, ()=>{
+    console.log("Server is running on port "+port);
+});
